7623fccb1293bc51208194ae9c3a91dcddae7526
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-02 23:39:53 -0800

    Add files via upload

---
+++ age-voting-checker.py
@@ -0,0 +1,49 @@
+age =18
+guess = int(input("guess the age"))
+if guess > age:
+ print('you are ready for vote')
+elif guess == age:
+ print('equal')
+else:
+ print('you are not ready for vote')
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+ 
+ 
+     
+     
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
