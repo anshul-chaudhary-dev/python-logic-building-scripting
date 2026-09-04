2ed2006ba1f4e69d8019683c5ae51a0fa7800080
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-04 09:35:34 -0800

    Add files via upload

---
+++ while-loop-token-counter.py
@@ -0,0 +1,11 @@
+print("Hallo evryone,we are starting")
+n=1
+c=1
+while(c==1):
+    print("token number",n,"mayplease come in")
+    c=input("continue?(0/10)")
+    c=int(c)
+    n=n+1
+    print("thank you,this is the end of our day")
+    
+
