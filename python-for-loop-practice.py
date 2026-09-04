bb7850910d19f6cd7349664291575d61ee64a8af
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-03 18:27:30 -0800

    Add files via upload

---
+++ python-for-loop-practice.py
@@ -0,0 +1,13 @@
+for I in range(10):
+    print(I)
+for I in range (71):
+    print("hi,how are you,i hope youare doing well")
+for I in range(65):
+    print("hi,you are number",I)
+    
+    
+    
+    
+    
+    
+    
