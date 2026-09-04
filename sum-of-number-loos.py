9712100695c07f2a87d7e1d8f10802ba73be004b
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-03 19:32:57 -0800

    Add files via upload

---
+++ sum-of-number-loos.py
@@ -0,0 +1,16 @@
+answer= 0
+print(answer)
+answer=answer+1
+print(answer)
+answer=answer+2
+print(answer)
+answer=answer+3
+print(answer)
+answer=answer+6
+print(answer)
+for I in range(11):
+    answer=answer+I
+    print(answer)
+    
+     
+    
