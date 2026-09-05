3132d25fad23be51803c55d59e6f3cc2e6fef47a
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-05 09:32:13 -0800

    Add files via upload

---
+++ list-part-one-practice.py
@@ -0,0 +1,18 @@
+t1="amit"
+t2="simran"
+t3="vidhya"
+i="sudarshan"
+#list
+shopping=["bread","coffee","sugar"]
+print(shopping)
+for i in range(3):
+    for item in shopping:
+        print(item)
+print(shopping)
+#append
+shopping.append("curd")
+print(shopping)
+#append
+shopping.append("shoes")
+print(shopping)
+
