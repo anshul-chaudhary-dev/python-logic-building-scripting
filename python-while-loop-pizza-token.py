4865202d389fae22ba2845e59bd170fe3d472819
anshul-chaudhary-dev <anshulchaudhary3003@gmail.com>
2026-09-05 02:58:11 -0800

    Add files via upload

---
+++ python-while-loop-pizza-token.py
@@ -0,0 +1,17 @@
+print("hallo everyone,we are staring now")
+n=1
+c=1
+while(c==1):
+    pizza_type=input("which pizza order")
+    print("token number",n,"your",pizza_type," pizza is ready!")
+    c=input("continue?(0/1)")
+    c=int(c)
+    n=n+1
+print("thank you ,this is the end of our day")
+
+   
+    
+    
+    
+    
+    
